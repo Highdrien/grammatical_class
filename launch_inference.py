4980a456a85e1343c436b587f313db19d4299682
yanis112 <yanisyanis545@gmail.com>
2023-12-22 00:11:15 +0100

    a

--- launch_inference.py
+++ launch_inference.py
@@ -22,6 +22,16 @@ def main() -> None:
     train(config)
 
 def find_classes(tenseur):
+    """
+    Finds the classes with the highest probability for each element in the tensor.
+
+    Args:
+        tenseur (torch.Tensor): The input tensor.
+
+    Returns:
+        list: A list of lists containing the indices of the classes with the highest probability for each element in the tensor.
+    """
+
     # Appliquer la fonction argmax sur le dernier axe du tenseur
     indices_max = torch.argmax(tenseur, dim=-1)
     
@@ -31,6 +41,16 @@ def find_classes(tenseur):
     return indices_max_liste
 
 def find_POS(list_index):
+    """
+    Finds the part-of-speech (POS) classes corresponding to the given list of indices.
+    
+    Args:
+        list_index (list): A list of indices representing the POS classes.
+        
+    Returns:
+        list: A list of POS classes corresponding to the given indices.
+    """
+    
     POS_CLASSES = ['PRON', 'AUX', 'VERB', 'DET', 'ADJ', 'NOUN', 'ADP', 'PROPN', 'NUM', 'CCONJ',
                '<PAD>', 'ADV', 'PART', 'INTJ', 'SYM', 'PUNCT', 'SCONJ', 'X', '_']
     POS = []
@@ -58,6 +78,7 @@ def inference(sentence,dictionary):
     sentence: list of string
     model: model
     dictionary: dict[string, int]
+    return: list of POS labels
     """
     #load config
     config = load_config(os.path.join('config', 'config.yaml'))
